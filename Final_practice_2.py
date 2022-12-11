7a4d45ac26cd5e22476b02a1752ea24c6a3302c9
unknown <bkrishna1114>
2022-12-11 16:33:08 +0530

    bala done this

--- Final_practice_2.py
+++ Final_practice_2.py
@@ -1,19 +1,22 @@
 import pandas as pd
-import numpy as np
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input , Output, State
-import plotly.express as px
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
+import plotly.express as px
 from dash import no_update
 
 #imprting data to the program...
-df = pd.read_csv('Automobiles.csv')
+df =  pd.read_csv('automobileEDA.csv', 
+                            encoding = "ISO-8859-1",
+                            )
 
 #creating application with dash module....
 app = dash.Dash(__name__)
 
+# REVIEW1: Clear the layout and do not display exception till callback gets executed
+app.config.suppress_callback_exceptions = True
 #adding features to the application...
 
 app.layout = html.Div(children = 
@@ -26,20 +29,24 @@ app.layout = html.Div(children =
     html.Div([
     
     #adding lable to the drop down ....
-    html.H3('Drive Wheels Type :',style={'margin-right':'2em','color':'#8EF3F9'}),
-    
-    #second Innter division....
+            html.Div(
+                html.H3('Drive Wheels Type :',style={'margin-right':'2em','color':'#8EF3F9'})
+                    ),
+            #second Innter division....
             dcc.Dropdown(id='demo-dropdown',
-                         options=[
-                                 {'label': 'Rear Wheel Drive', 'value': 'rwd'},
-                                 {'label': 'Front Wheel Drive', 'value': 'fwd'},
-                                 {'label': 'Four Wheel Drive', 'value': '4wd'}],
-                                 value='rwd'),
+                                 options=[
+                                         {'label': 'Rear Wheel Drive', 'value': 'rwd'},
+                                         {'label': 'Front Wheel Drive', 'value': 'fwd'},
+                                         {'label': 'Four Wheel Drive', 'value': '4wd'}
+                                         ],
+                                         value='rwd'),
             #adding 2 inner divisoins for the graphs....
             html.Div([
-                html.Div([],id='plot1'),
-                html.Div([],id='plot2')
-                ],style={'display':'flex'})
+                html.Div([ ],id='plot1'),
+                html.Div([ ],id='plot2')
+                ],
+                
+                style={'display':'flex'})
             #closing 2 inner division....here
     
     
@@ -49,25 +56,29 @@ app.layout = html.Div(children =
 
 
 #Place to add @app.callback Decorator
-@app.callback([Output(component_id='plot1', component_property='children'),
-               Output(component_id='plot2', component_property='children'),
-               Input(component_id='demo-dropdown', component_property='value')
-               ])
+@app.callback(
+                [Output(component_id='plot1', component_property='children'),
+                 Output(component_id='plot2', component_property='children')],
+              Input(component_id='demo-dropdown', component_property='value')
+              )
 #defining the callback function...
-def display_charts(value):
-    df2 = df[df['drive-wheels']==value].groupby(['drive-wheels','body-style'],\
-                                                as_index='False').mean()
+def display_selected_drive_charts(value):
+    
+    df2 = df[df['drive-wheels']==value].groupby(['drive-wheels','body-style'],as_index=False).mean()
     df2 =df2
     
     #figures...
     fig1 = px.pie(df2,values='price',names='body-style',title='Pie Chart')
-    fig1 = px.bar(df2,y='price',x='body-style',title='Bar Chart')
+    fig2 = px.bar(df2,x='body-style',y='price',title='Bar Chart')
     
+  
     #returning the graphs...
     return [dcc.Graph(figure=fig1),
-            dcc.Graph(figure=fig2)]
+            dcc.Graph(figure=fig2) ]
 
+print(df.columns)
 
 #calling the server...
 if __name__ =='__main__':
     app.run_server()
+
